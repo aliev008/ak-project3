160e9aa60ccae42ccd8c856a123ff669a2f6e2ca
aliev008 <hfe.as@yandex.ru>
2022-10-11 13:14:02 +0300

    feat: added showing more cards feature

--- src/js/utils/showMoreStyles.js
+++ src/js/utils/showMoreStyles.js
@@ -1,7 +1,21 @@
 export const showMoreStyles = (trigger, styles) => {
-    const card = document.querySelectorAll(styles);
-    cards.forEach(card => {
-        card.classList.add('animated');
-        
+  const cards = document.querySelectorAll(styles),
+    btn = document.querySelector(trigger);
+  cards.forEach((card) => {
+    card.classList.add("animated", "fadeInUp");
+  });
+
+  btn.addEventListener("click", (e) => {
+    cards.forEach((card) => {
+      card.classList.remove("hidden-lg", "hidden-md", "hidden-sm", "hidden-xs");
+      card.classList.add(
+        "col-sm-3",
+        "col-sm-offset-0",
+        "col-xs-10",
+        "col-xs-offset-1"
+      );
     });
-}
+
+    btn.style.display = "none";
+  });
+};
