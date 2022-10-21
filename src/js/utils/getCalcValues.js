a20380b42e54c14b1926678da9ff9d3b07511b37
aliev008 <hfe.as@yandex.ru>
2022-10-21 14:19:28 +0300

    fix: minor refactoring

--- src/js/utils/getCalcValues.js
+++ src/js/utils/getCalcValues.js
@@ -2,20 +2,16 @@ import { getData } from "../services/requests";
 
 export const getCalcValues = async (selector) => {
   let result;
+  try {
+    const data = await getData("./assets/db.json");
+    data.calcValues.forEach((item) => {
+      if (item.id === selector) {
+        result = item.values[document.querySelector(selector).value];
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 
-  const getValue = async (selector) => {
-    try {
-      const data = await getData("./assets/db.json");
-      data.calcValues.forEach((item) => {
-        if (item.id === selector) {
-          result = item.values[document.querySelector(selector).value];
-        }
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  await getValue(selector);
-  return result;
+  return await result;
 };
