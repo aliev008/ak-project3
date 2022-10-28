e28ae5b1cb43567addf4c1bd517db32d95c174da
aliev008 <hfe.as@yandex.ru>
2022-10-29 01:28:26 +0300

    feat: added smooth scroll to an ancor with scrollIntoView method

--- src/js/utils/smoothScroll.js
+++ src/js/utils/smoothScroll.js
@@ -1,49 +1,11 @@
 export const smoothScroll = (elemSelector) => {
-  const elem = document.querySelector(elemSelector),
-    rootDocument = document.documentElement,
-    body = document.body;
+  const elem = document.querySelector(elemSelector);
 
-  elem.addEventListener("click", function (e) {
-    const scrollTop = Math.round(rootDocument.scrollTop || body.scrollTop);
 
-    if (this.hash !== "") {
-      e.preventDefault();
-      let targetElem = document.querySelector(this.hash),
-        targetElemScroll = 0;
-        
-      while (targetElem.offsetParent) {
-        targetElemScroll += targetElem.offsetTop;
-        targetElem = targetElem.offsetParent;
-      }
-
-      targetElemScroll = Math.round(targetElemScroll);
-      scrolling(scrollTop, targetElemScroll, this.hash);
-    }
-  });
-
-  const scrolling = (start, end, hash) => {
-    let timeInterval = 1,
-      prevScrollTop,
-      speed = start > end ? -30 : 30;
-
-    const action = setInterval(() => {
-      let scrollTop = Math.round(rootDocument.scrollTop || body.scrollTop);
-      if (
-        prevScrollTop === scrollTop ||
-        (end > start && scrollTop >= end) ||
-        (end < start && scrollTop <= end)
-      ) {
-        clearInterval(action);
-        history.replaceState(
-          history.state,
-          document.title,
-          location.href.replace(/#.*$/g, "") + hash
-        );
-      } else {
-        body.scrollTop += speed;
-        rootDocument.scrollTop = rootDocument.scrollTop + speed;
-        prevScrollTop = scrollTop;
-      }
-    }, timeInterval);
-  };
+  elem.addEventListener('click', function (e) {
+    const destination = document.querySelector(this.hash);
+    e.preventDefault();
+    destination.scrollIntoView({behavior: 'smooth'});
+    console.log(destination);
+  })
 };
